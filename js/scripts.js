da4d9927ddd8cbdb7487afecd47bae2fb008569f
Carlos Román <croman@CRR-2.local>
2018-03-09 15:29:27 -0600

    Arreglar slider en movil.

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,43 @@ $(document).ready(function() {
   // Initialize What I Do section.
   document.getElementById("what-i-do-pd").click() ;
   // Initialize slider.
-  $("#lightSlider").lightSlider({
+  $("#lightSliderDesktop").lightSlider({
+    item: 1,
+    autoWidth: true,
+    slideMove: 1, // slidemove will be 1 if loop is true
+    slideMargin: 10,
     keyPress: true,
-    enableTouch: true,
-    enableDrag: true,
-    item: 3,
-    slideMargin: 30,
     pager: false,
-  }) ;
+    enableTouch: true,
+  });
+
+  $("#lightSliderMovil").lightSlider({
+    item: 1,
+    autoWidth: false,
+    slideMove: 1, // slidemove will be 1 if loop is true
+    slideMargin: 10,
+    pager: false,
+    gallery: true,
+    enableTouch: true,
+    enableDrag:true,
+    freeMove:true,
+    swipeThreshold: 40,
+    responsive : [ {
+        breakpoint:800,
+        settings: {
+          item:3,
+          slideMove:1,
+          slideMargin:6,
+          }
+      }, {
+        breakpoint:480,
+        settings: {
+          item:1,
+          slideMove:1
+        }
+      }
+    ],
+  });
 });
 
 // What I Do Style Changing
